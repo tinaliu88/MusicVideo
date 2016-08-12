023a011c0790776fdbbf1d59c56604f12ae5e795
Tina Liu <tinaliu@192-168-1-2.tpgi.com.au>
2016-08-12 14:30:34 +1000

    Part 1 - created the groupings

---
+++ MusicVideo/APIManager.swift
@@ -0,0 +1,16 @@
+//
+//  APIManager.swift
+//  MusicVideo
+//
+//  Created by Tina Liu on 12/08/2016.
+//  Copyright © 2016 SunnyFridayApps. All rights reserved.
+//
+
+import Foundation
+
+class APIManager {
+    
+    func loadData(urlString : String, completion: (result: String) -> ()) {
+        
+    }
+}
